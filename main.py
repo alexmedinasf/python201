7b1b72a5e298ba81c6c30b1aae476c8505340515
alexmedinasf <alexsales001@hotmail.com>
2024-08-20 09:21:19 -0300

    main structure of the game

---
+++ main.py
@@ -0,0 +1,27 @@
+def new_game():
+    pass
+#-------------------------------------------------
+def check_answers():
+    pass
+#-------------------------------------------------
+def check_score():
+    pass
+#-------------------------------------------------
+def play_again():
+    pass
+#-------------------------------------------------
+
+questions = { "What year this project was created?: ": "B",
+              "What is my GitHUB nickname?: ":"A",
+              "What am I using to code this?: ":"A",
+              "Is the sky blue?: ":"C",
+              "Are you having fun?: ":"B"
+}
+
+options =[["A.  2038", "B.  2024", "C.   1993"],
+          ["A.  alexmedinasf", "B.  medinasfalex", "C.   none"],
+          ["A.   Python", "B.   C++", "C.  JavaScript"],
+          ["A.  No", "B.  Idk, Idc", "C.   Ofc dude lol"],
+          ["A.  No", "B.  BEST GAME EVER", "C.  Who are you??"]]
+
+new_game()
